f301a884d358f927e50502eb3232f1b796a0423b
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-12-14 23:48:50 -0300

    s18_hash.008

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ let buttons = <document.querySelectorAll('#buttons-container button')
 let messageContainer = document.querySelector('#message')
 let messageText = document.querySelector('#message p')
 let secondPlayer
+
+// Move counter ( contador de jogadas )
+
+let player1 = 0
+let player2 = 0
+
+// Adding the click event to boxes ( Adicionando o evento click aos boxes )
