cf0b304607378fa43b49f562c778676ae0c8f557
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-12-16 07:54:00 -0300

    s18_hash.011

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let x = document.querySelector('.x')
 let o = document.querySelector('.o')
 let boxes = document.querySelectorAll('.box')
-let buttons = <document.querySelectorAll('#buttons-container button')
+let buttons = document.querySelectorAll('#buttons-container button')
 let messageContainer = document.querySelector('#message')
 let messageText = document.querySelector('#message p')
 let secondPlayer
@@ -18,13 +18,31 @@ for (let i = 0; i < boxes.length; i++) {
 // When someone clicks on the box ( Quando alguém clicar na caixa )
 
   boxes[i].addEventListener('click', function() {
-    let el
-    if (player1 == player2) {
-      el = x    
-    } else {
-      el = o
+    let el = checkEl(player1, player2)
+
+    // Checks if it already has X or O ( Verifica se já tem X ou O )
+
+    if(this.childNodes.length == 0) {
+      let cloneEl = el.cloneNode(true)
+      this.appendChild(cloneEl)
+  
+        // compute the game ( Computar a jogada )
+    
+        if(player1 == player2) {
+          player1++
+        } else {
+          player2++
+        }
     }
-    let cloneEl = el.cloneNode(true)
-    this.appendChild(cloneEl)
   })
+}
+
+// see who will play ( Ver quem vai jogar )
+function checkEl(player1, player2) {
+  if (player1 == player2) {
+    el = x    
+  } else {
+    el = o
+  }
+  return el
 }
