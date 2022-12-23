544d7e9fe3837068e32c1aea7c813b3c0b4fd007
Anderson Olliver <85455963+andersondeoliveirasantos@users.noreply.github.com>
2022-12-23 00:14:46 -0300

    s18_hash.021

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 let x = document.querySelector('.x')
-
 let o = document.querySelector('.o')
-
 let boxes = document.querySelectorAll('.box')
-
 let buttons = document.querySelectorAll('#buttons-container button')
-
 let messageContainer = document.querySelector('#message')
-
 let messageText = document.querySelector('#message p')
-
 let secondPlayer
 
 // Move counter ( contador de jogadas )
